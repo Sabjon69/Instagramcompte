46086684ec093ecac5f68078eb1a19f39f334074
UTILISATEUR <utilisateur@sabjon.local>
2026-01-12 15:17:15 +0100

    Fix register route

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,18 @@
-const db = require("../db/connection");
+const mysql = require("mysql2");
+
+// Connexion MySQL
+const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
+});
 
 module.exports = (req, res) => {
     const { username, email } = req.body;
+
+    console.log("Reçu :", username, email);
 
     if (!username || !email) {
         return res.status(400).send("Champs manquants");
@@ -11,11 +22,11 @@ module.exports = (req, res) => {
 
     db.query(sql, [username, email], (err, result) => {
         if (err) {
-            console.error("Erreur MySQL :", err);
+            console.error("Erreur SQL :", err);
             return res.status(500).send("Erreur serveur");
         }
 
-        console.log("Nouvel utilisateur enregistré :", username, email);
+        console.log("Utilisateur ajouté !");
         res.send("Inscription réussie !");
     });
 };
